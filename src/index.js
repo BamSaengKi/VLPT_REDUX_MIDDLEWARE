279df25e1231922d1e5742a543272386c8f1c2e3
bamsaengki <bamsaengki@gmail.com>
2023-03-24 19:02:51 +0900

    [FistComit]6.API재로딩 해결

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./modules";
+import logger from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
+import ReduxThunk from "redux-thunk";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(ReduxThunk, logger))
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/PostListPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import PostListContainer from "../containers/PostListContainer";
+
+const PostListPage = () => {
+  return (
+    <>
+      <PostListContainer />
+      <Outlet />
+    </>
+  );
+};
+
+export default PostListPage;
